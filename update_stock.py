40297318a73ed8b623b4b4f0dfd962213178babb
dtchien401 <dtchien401@gmail.com>
2026-03-12 09:51:51 +0700

    Update update_stock.py

--- update_stock.py
+++ update_stock.py
@@ -1,7 +1,26 @@
-from vnstock import Quote
+import json
+import os
 import pandas as pd
+import gspread
+from google.oauth2.service_account import Credentials
+from vnstock import Quote
 
-symbols = ["HPG","VCG","VPB","POW","SZC","PVD","PLX","VIC", "GVR", "GMD"]
+# đọc secret từ github
+service_account_info = json.loads(os.environ['GOOGLE_SERVICE_ACCOUNT'])
+
+scopes = [
+    "https://www.googleapis.com/auth/spreadsheets",
+    "https://www.googleapis.com/auth/drive"
+]
+
+creds = Credentials.from_service_account_info(service_account_info, scopes=scopes)
+
+gc = gspread.authorize(creds)
+
+# mở google sheet
+sheet = gc.open("Data Cp").sheet1
+
+symbols = ["HPG","VCG","VPB","POW","SZC","PVD","PLX","VIC"]
 
 for symbol in symbols:
 
@@ -12,5 +31,18 @@ for symbol in symbols:
         interval="1D"
     )
 
-    print(symbol)
-    print(df.tail(1))
+    last = df.tail(1)
+
+    row = [
+        symbol,
+        str(last.iloc[0]["time"]),
+        last.iloc[0]["open"],
+        last.iloc[0]["high"],
+        last.iloc[0]["low"],
+        last.iloc[0]["close"],
+        last.iloc[0]["volume"]
+    ]
+
+    sheet.append_row(row)
+
+    print("Updated:", symbol)
